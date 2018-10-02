9d3f581bcb7f8f58f295dcc2e1245e1d9c18df96
Diphladenia <43388850+Diphladenia@users.noreply.github.com>
2018-10-02 17:44:17 +0200

    Image_reproduced

--- sketch.js
+++ sketch.js
@@ -4,8 +4,23 @@ function preload(){
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(40);
+  angleMode(DEGREES);
+
+  frameRate(12);
 }
 
 function draw() {
   // put drawing code here
+  translate(width/2,height/2);
+  //rotate(frameCount*3);
+
+
+  stroke(lerpColor(color('#58a3b3'),color('#eeecbb'), frameCount/120));
+  line(100,0,(cos(frameCount*3)*100),(sin(frameCount*3)*100));
+// Stops the draw function if a certain condition avaluates true
+ if (frameCount == 120) {
+   noLoop();
+ }
 }
